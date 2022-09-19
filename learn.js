590ce29bd1fdc146adf11edb92414beba781220c
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-19 11:06:27 +0530

    init

--- learn.js
+++ learn.js
@@ -1,7 +1,11 @@
 console.log("Start");
 
-document.getElementById("btn").addEventListener("click", function cb() {
-  console.log("Callback");
+setTimeout(function cbT() {
+  console.log("CB SetTimeout");
+}, 5000);
+
+fetch("https://api.publicapis.org/entries").then(function cbF() {
+  console.log("CB Netflix");
 });
 
 console.log("End");
