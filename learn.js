def85158ad178be2da5bb2a43716d46e2dd8c261
drcount-root <about.subhamsahu@gmail.com>
2022-09-19 08:19:44 +0530

    Initial

---
+++ learn.js
@@ -0,0 +1,7 @@
+console.log("Start");
+
+setTimeout(function cb() {
+  console.log("Callback");
+}, 5000);
+
+console.log("End");
---
+++ t.js
@@ -0,0 +1,9 @@
+let x = 4;
+let y = x--;
+console.log(x);
+console.log(y);
+
+let count = 0;
+count--;
+console.log(count);
+console.log(count--);
