2e321ea58d733228dfe943c74bb90578c1ced31c
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-20 19:57:09 +0530

    20th Sept

--- learn.js
+++ learn.js
@@ -4,8 +4,22 @@ setTimeout(function cbT() {
   console.log("CB SetTimeout");
 }, 5000);
 
-fetch("https://api.publicapis.org/entries").then(function cbF() {
-  console.log("CB Netflix");
-});
+// fetch("https://api.publicapis.org/entries").then(function cbF() {
+//   console.log("CB Netflix");
+// });
+
+function fN() {
+  for (var i = 1; i <= 5; i++) {
+    function count(i) {
+      setTimeout(function () {
+        console.log(i);
+      }, i * 1000);
+    }
+
+    count(i);
+  }
+}
+
+fN();
 
 console.log("End");
--- t.js
+++
@@ -1,9 +0,0 @@
-let x = 4;
-let y = x--;
-console.log(x);
-console.log(y);
-
-let count = 0;
-count--;
-console.log(count);
-console.log(count--);
