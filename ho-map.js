056c91a48100e970eeb41466b62c38c894b398b0
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-21 20:39:12 +0530

    X

--- ho-map.js
+++ ho-map.js
@@ -4,8 +4,13 @@ const area = function (radius) {
   return Math.PI * radius * radius;
 };
 
-// console.log(radius.map(area));
-
+console.log("---------- using built-in map()----------");
+console.log(radius.map(area));
+//
+//
+//
+//
+// Own map() func
 Array.prototype.calculate = function (logic) {
   const output = [];
   for (let i = 0; i < this.length; i++) {
@@ -14,5 +19,112 @@ Array.prototype.calculate = function (logic) {
   return output;
 };
 
+console.log("---------- using own map()----------");
 console.log(radius.calculate(area));
- 
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+//  map()
+
+console.log("\n---------- using map()----------");
+
+const arr = [5, 1, 3, 2, 6];
+
+const cbDouble = (param) => param * 2;
+const resultArr = arr.map(cbDouble);
+console.log(resultArr);
+
+const doub = arr.map((i) => i * 2);
+console.log(doub);
+
+const binary = arr.map((b) => b.toString(2));
+console.log(binary);
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//  filter()
+
+console.log("\n---------- using filter()----------");
+
+const arrF = [3, 1, 4, 5, 2];
+const resArr = arrF.filter((i) => i % 2 !== 0);
+
+console.log(resArr);
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//  reduce()
+
+console.log("\n---------- using reduce()----------");
+
+const arrR = [3, 1, 4, 5, 2];
+function findSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(findSum(arrR));
+//
+//
+//
+const out = arrR.reduce((acc, curr) => acc + curr);
+
+console.log(out);
+
+//
+//
+//
+//
+//
+//
+//
+
+// Max value from an array using reduce
+
+const arrX = [2, -5, 99, 12, 46];
+
+const findMax = arrX => {
+  let max = arrX[0];
+  for(let i = 1; i < arrX.length; i++){
+    if(arrX[i] > max) max = arrX[i];    
+  }
+  return max;
+}
+
+console.log(findMax(arrX));
